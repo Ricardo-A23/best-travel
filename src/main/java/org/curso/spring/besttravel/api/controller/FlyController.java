874d22f7a32f721cc16ca7e17e2810d3cad34d55
Ricardo Alvarez Martinez <aamr020403@gmail.com>
2026-01-19 10:29:36 -0600

    se agrego el controller de fly

--- src/main/java/org/curso/spring/besttravel/api/controller/FlyController.java
+++ src/main/java/org/curso/spring/besttravel/api/controller/FlyController.java
@@ -5,10 +5,11 @@ import org.curso.spring.besttravel.api.models.dto.response.FlyResponse;
 import org.curso.spring.besttravel.infrastructure.abstract_service.IFlyService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/flights")
@@ -22,4 +23,25 @@ public class FlyController {
             @RequestParam int page, @RequestParam int size) {
         return ResponseEntity.ok(flyService.getAll(page, size));
     }
+
+    @GetMapping("/search/price-menor")
+    public ResponseEntity<List<FlyResponse>> findByLessPrice(@RequestParam BigDecimal price) {
+        return ResponseEntity.ok(flyService.selectLessPrice(price));
+    }
+
+    @GetMapping("/search/price-range")
+    public ResponseEntity<List<FlyResponse>> findByBetweenPrice(@RequestParam BigDecimal min, @RequestParam BigDecimal max) {
+        return ResponseEntity.ok(flyService.selectBetweenPrice(min, max));
+    }
+
+    @GetMapping("/search/by-route")
+    public ResponseEntity<List<FlyResponse>> findByOriginDestination(@RequestParam String origin, @RequestParam String destination) {
+        return ResponseEntity.ok(flyService.selectOriginDestiny(origin, destination));
+    }
+
+    @GetMapping("/search/{ticketId}")
+    public ResponseEntity<FlyResponse> findByTicketId(@PathVariable UUID ticketId) {
+        return ResponseEntity.ok(flyService.findByTicketId(id));
+    }
+
 }
