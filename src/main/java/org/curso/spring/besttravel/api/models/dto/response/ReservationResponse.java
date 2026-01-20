bd56d9fdf78dccebe96fdbcc38a36815bbf683f4
Ricardo Alvarez Martinez <aamr020403@gmail.com>
2026-01-20 13:08:05 -0600

    lombok agregado

--- src/main/java/org/curso/spring/besttravel/api/models/dto/response/ReservationResponse.java
+++ src/main/java/org/curso/spring/besttravel/api/models/dto/response/ReservationResponse.java
@@ -1,13 +1,24 @@
 package org.curso.spring.besttravel.api.models.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ReservationResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dateStart;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dateEnd;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime dateTimeReservation;
     private HotelResponse hotel;
     private BigDecimal price;
